3b6f9db9eb54ce3b79540797d55d232457b9a48b
Iyanu456 <oyerindei13@gmail.com>
2023-04-21 13:31:49 +0100

    default message

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def show_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def show_number_template(n):
-    """ Prints a message when the show_number_template route is requested """
+    """ Display an HTML page if and only if n is an integer """
     return (render_template("5-number.html", number=n))
 
 
